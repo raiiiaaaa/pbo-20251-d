df5cc1664f2e0567a1a56cd6707df5b2f9c4de80
raiiiaaaa <aarayyaseva6969@gmail.com>
2025-11-07 20:17:49 +0700

    ini pakai Construct dan Destruct

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -4,6 +4,14 @@ class LuasLingkaran {
     public const phi = 3.14;
     public int $jari;
 
+    public function __construct($IsiJari = 1) {
+        $this->jari = $IsiJari;
+    }
+
+    public function __destruct() {
+        echo "<br>";
+        echo "udah ah cape";
+    }
     public function tampil($nama = 'ban') {
         $rumus = LuasLingkaran::phi * $this->jari * $this->jari;
         echo "Lingkaran $nama hasilnya adalah: $rumus";
@@ -15,8 +23,7 @@ class LuasLingkaran {
     }
 }
 
-$lingkaran = new LuasLingkaran();
-$lingkaran->jari = 7;
+$lingkaran = new LuasLingkaran(7);
 $lingkaran->tampil('kue'); // panggil method
 LuasLingkaran::testing()
 ?>

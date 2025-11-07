c78f8a0bbc643fc3462a4696d775f5d9198787fc
raiiiaaaa <aarayyaseva6969@gmail.com>
2025-11-07 20:13:44 +0700

    ini pakai Method dan Static

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -3,11 +3,20 @@
 class LuasLingkaran {
     public const phi = 3.14;
     public int $jari;
+
+    public function tampil($nama = 'ban') {
+        $rumus = LuasLingkaran::phi * $this->jari * $this->jari;
+        echo "Lingkaran $nama hasilnya adalah: $rumus";
+    }
+    
+    public static function testing() {
+        echo "<br>";
+        echo "ini dari static";
+    }
 }
 
 $lingkaran = new LuasLingkaran();
 $lingkaran->jari = 7;
-
-$rumus = LuasLingkaran::phi * $lingkaran->jari * $lingkaran->jari;
-echo 'hasilnya adalah'. $rumus;
+$lingkaran->tampil('kue'); // panggil method
+LuasLingkaran::testing()
 ?>
